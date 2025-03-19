3fb3df25a9f35db3ba181fa867c1aa7a3a6ccbc2
Matěj H <qop727@gmail.com>
2025-03-19 08:54:41 +0100

    FE vue.js

--- BE/1_be_install_fedora.sh
+++ BE/1_be_install_fedora.sh
@@ -2,6 +2,8 @@
 
 python3 -m venv venv                                    # Creation of virtual environment.
 
+sudo chmod +x ~/library_project/app.py                  # Adding execute permissions to BE scrypt.
+
 source venv/bin/activate                                # Activation of virtual environment.
 
 pip install Flask flask_sqlalchemy flask_cors PyMySQL   # Installation of needed packages.
--- FE/1_fe_install_fedora.sh
+++ FE/1_fe_install_fedora.sh
@@ -4,8 +4,12 @@ sudo dnf install nodejs npm                             # Install node.js and no
 
 sudo npm install -g @vue/cli                            # Install vue CLI.
 
+cd ~/qop/GitHub/library-project/FE                      # Change directory to where I want to create new project.
+
 sudo vue create my-vue-app                              # Create new project.
 # During creation choose the TypeScript version.
+
+sudo chown -R l_qopd:l_qopd my-vue-app                  # Change ownership to current user to have permissions to change configs easily.
 
 cd my-vue-app                                           # Go to project folder.
 
